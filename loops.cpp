eca06ee5c910880afa7063cf09e6a368255d4d9e
Bharat Sharma <87943440+Bharat-1Sharma@users.noreply.github.com>
2022-02-09 20:33:18 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+   /* char ch;
+    cout<<"Enter the character";
+    cin>>ch;;
+    if(ch>='a'&&ch<='z'){
+        cout<<"Lowercase";}
+
+        else if(ch>='A'&&ch<='Z')
+        {
+            cout<<"Uppercase";
+        }else if(ch>='0'&&ch<='9'){
+            cout<<"Numeric";4
+        }
+     */   
+int n;
+cout <<"Enter the Number ";
+cin>>n;
+int i=1;
+int sum=0;
+while(i<=n){
+sum+=i;
+
+
+i++;
+
+}
+   cout<<sum;        
+    return 0;
+}
